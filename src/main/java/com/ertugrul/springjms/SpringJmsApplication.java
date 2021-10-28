a15b1d3e4ffbbf7293bdcf5d9e37247e3047401e
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2021-10-28 10:37:58 +0300

    Initial commit

---
+++ src/main/java/com/ertugrul/springjms/SpringJmsApplication.java
@@ -0,0 +1,13 @@
+package com.ertugrul.springjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ertugrul/springjms/SpringJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ertugrul.springjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
