7b72ae97f05bb560a5b392955790a9c04a7111df
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2021-10-28 12:13:23 +0300

    config package and jms-config created.

--- src/main/java/com/ertugrul/springjms/SpringJmsApplication.java
+++ src/main/java/com/ertugrul/springjms/SpringJmsApplication.java
@@ -1,12 +1,24 @@
 package com.ertugrul.springjms;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.impl.ActiveMQServerImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJmsApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        ActiveMQServer server = new ActiveMQServerImpl(new ConfigurationImpl()
+                .setPersistenceEnabled(false)
+                .setJournalDirectory("target/data/journal")
+                .setSecurityEnabled(false)
+                .addAcceptorConfiguration("ivnm", "vm://0"));
+
+        server.start();
+
         SpringApplication.run(SpringJmsApplication.class, args);
     }
 
